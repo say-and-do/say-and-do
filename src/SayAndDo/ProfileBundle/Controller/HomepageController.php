5966904a786832dbc53f68620b60ed0ec20b0e0c
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-06-29 00:31:32 +0300

    Homepage implemented

--- src/SayAndDo/ProfileBundle/Controller/HomepageController.php
+++ src/SayAndDo/ProfileBundle/Controller/HomepageController.php
@@ -2,12 +2,34 @@
 
 namespace SayAndDo\ProfileBundle\Controller;
 
+use Doctrine\ORM\EntityRepository;
+use SayAndDo\ProfileBundle\Service\ProfileService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomepageController extends Controller
 {
     public function homepageAction()
     {
-        return $this->render('SayAndDoProfileBundle:Homepage:homepage.html.twig');
+        /** @var ProfileService $profileService */
+        $profileService = $this->get('sd_profile.service');
+
+        /** @var EntityRepository $repo */
+        $repo     = $this->get('doctrine.orm.default_entity_manager')->getRepository('SayAndDoProfileBundle:Profile');
+        $profiles = $repo->findBy([], ['points' => 'DESC'], 5);
+        $profiles = array_map(
+            function ($profile) use ($profileService) {
+                return [
+                    'profile'           => $profile,
+                    'tasks_confirmed'   => $profileService->getTasksConfirmed($profile),
+                    'tasks_in_progress' => $profileService->getTasksInProgress($profile),
+                    'tasks_done'        => $profileService->getTasksDone($profile),
+                ];
+            },
+            $profiles
+        );
+        return $this->render(
+            'SayAndDoProfileBundle:Homepage:homepage.html.twig',
+            ['profiles' => $profiles]
+        );
     }
 }
