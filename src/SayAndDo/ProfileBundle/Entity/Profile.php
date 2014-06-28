f63f7f03fcd5d9d06822909007a83453d14a0898
Edvinas Kručas <edv.krucas@gmail.com>
2014-06-28 12:27:58 +0300

    Added basic profile entity

---
+++ src/SayAndDo/ProfileBundle/Entity/Profile.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SayAndDo\ProfileBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Profile
+ */
+class Profile
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @var string
+     */
+    private $title;
+
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Profile
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
