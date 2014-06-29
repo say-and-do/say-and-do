7acf58ab4778b982fbe54169c4b997ae96fd8b58
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-06-29 13:11:51 +0300

    disable generator bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,7 @@ class AppKernel extends Kernel
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+//            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
